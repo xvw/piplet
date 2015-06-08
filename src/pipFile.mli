80a3d2da7810ba0c748b0e168471f8b007d6e07e
xvw <xavier.vdw@gmail.com>
2015-06-08 17:33:46 +0200

    Append File API

---
+++ src/pipFile.mli
@@ -0,0 +1,8 @@
+(* File API *)
+val read : string -> string
+val read_lines : string -> string list
+val write : ?chmod:int -> string -> string -> unit
+val append : ?chmod:int -> string -> string -> unit
+val prepend : ?chmod:int -> string -> string -> unit
+val remove : string -> unit
+val basename : string -> string
--- src/piplet.ml
+++ src/piplet.ml
@@ -1,5 +1,6 @@
 module Regexp  = PipRegexp
 module Date    = PipDate
 module Channel = PipChannel
+module File    = PipFile
 module Command = PipCommand
 module Git     = PipGit
