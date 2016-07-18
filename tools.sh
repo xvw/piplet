81fdbb02b2ac334022a9efdb494958fdc0d1f378
xvw <xavier.vdw@gmail.com>
2016-07-18 08:00:00 +0200

    Restore build system

---
+++ tools.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Collection of little tools for oasis Management
+if [ "$1" = "clean" ];
+then
+    echo "Clean emacs files"
+    rm -rf *~
+    rm -rf */*~
+    rm -rf \#*
+    rm -rf */\#*
+    make clean
+    make distclean
+
+elif [ "$1" = "setup-oasis" ];
+then
+     echo "Setup Oasis"
+     oasis setup -setup-update dynamic
+elif [ "$1" = "run" ];
+then
+    echo "Run OCaml with piplet"
+    make
+    ocaml -I _build/src unix.cma str.cma Piplet.cma
+    echo "Bye"
+
+else
+    ocamlc -I _build/src unix.cma str.cma Piplet.cma $*
+fi
