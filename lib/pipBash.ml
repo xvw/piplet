01f27618a595529969de604c2f54015420814fb7
xvw <xavier.vdw@gmail.com>
2016-07-19 08:00:00 +0200

    Initialize process execution

--- lib/pipBash.ml
+++ lib/pipBash.ml
@@ -1,1 +1,2 @@
-
+let execute_channel = Unix.open_process_in
+let close_channel_execution = Unix.close_process_in
---
+++ lib/pipBash.mli
@@ -0,0 +1,2 @@
+val execute_channel: string -> in_channel
+val close_channel_execution : in_channel -> Unix.process_status
