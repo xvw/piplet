e325429fc227b62d7dcf197f177f3f5058df2fb6
xvw <xavier.vdw@gmail.com>
2016-07-23 10:32:49 +0200

    Add some regex function

--- lib/pipRegex.ml
+++ lib/pipRegex.ml
@@ -1,4 +1,12 @@
 type regex = string
+type source = string
 
 let reg = Str.regexp
+
 let according r = Str.string_match (reg r)
+let is_matching = according
+
+let replace pattern source =
+  Str.global_replace (reg pattern) source
+
+let purge pattern = replace pattern ""
