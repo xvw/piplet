6bb09d2c12c1e69e31ef0aac28d9360165289e65
xvw <xavier.vdw@gmail.com>
2016-07-23 11:24:20 +0200

    I should use OLMI :v

--- lib/pip.ml
+++ lib/pip.ml
@@ -1,2 +1,25 @@
 let id x = x
 let flip f x y = f y x
+
+module List =
+struct
+
+  include List
+  open PipMake.Monad
+
+  module Req = Join (
+    struct
+      type 'a t = 'a list
+      let return x = [x]
+      let fmap = map
+      let join = flatten
+    end)
+
+  include Plus
+      (Req)
+      (struct
+        type 'a t = 'a list
+        let mempty = []
+        let mplus = append
+      end)
+end
