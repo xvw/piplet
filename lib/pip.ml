e2a7469b35867cf8c73a832c1aca1da19a5f2b13
xvw <xavier.vdw@gmail.com>
2016-07-24 08:00:00 +0200

    Append pervasives function

--- lib/pip.ml
+++ lib/pip.ml
@@ -1,25 +1,6 @@
-let id x = x
-let flip f x y = f y x
-
-module List =
-struct
-
-  include List
-  open PipMake.Monad
-
-  module Req = Join (
-    struct
-      type 'a t = 'a list
-      let return x = [x]
-      let fmap = map
-      let join = flatten
-    end)
-
-  include Plus
-      (Req)
-      (struct
-        type 'a t = 'a list
-        let mempty = []
-        let mplus = append
-      end)
-end
+let id     x     = x
+let flip   f x y = f y x
+let ( % )  f g x = f (g x)
+let ( %> ) f g x = g (f x)
+let ( $ )  f x   = f x
+let tap    f x   = let _ = f x in x
